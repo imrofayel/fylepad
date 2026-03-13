b1e10dcc965aae3fb91f9658f0e1e241e2b9696f
imrofayel <adamrofayel@gmail.com>
2026-03-13 11:42:37 +0500

    Remove `components.json` and `CODE_OF_CONDUCT.md` files, and apply minor formatting adjustments in `app.vue`.

--- app/app.vue
+++ app/app.vue
@@ -1,23 +1,23 @@
 <template>
-  <Analytics />
-  <VitePwaManifest />
+  <Analytics/>
+  <VitePwaManifest/>
   <UApp>
     <NuxtLayout>
-      <NuxtPage />
+      <NuxtPage/>
     </NuxtLayout>
   </UApp>
 </template>
 
 <script setup>
-import { inject } from "@vercel/analytics";
+import { inject } from '@vercel/analytics';
 
 inject();
 
 useSeoMeta({
-  title: "fylepad - your dream notepad!",
-  ogTitle: "fylepad - your dream notepad!",
-  description: "A minimal notepad with literally everything you need.",
-  ogDescription:
-    "An aesthetic notepad for effortless note-taking with AI. Enjoy rich editing, auto-save, multi-tab support, Mermaid / PlantUML diagrams, tables, code-blocks and much more — all in one.",
-});
+  title: 'fylepad - your dream notepad!',
+  ogTitle: 'fylepad - your dream notepad!',
+  description: 'A minimal notepad with literally everything you need.',
+  ogDescription: 'An aesthetic notepad for effortless note-taking with AI. Enjoy rich editing, auto-save, multi-tab support, Mermaid / PlantUML diagrams, tables, code-blocks and much more — all in one.',
+})
+
 </script>
