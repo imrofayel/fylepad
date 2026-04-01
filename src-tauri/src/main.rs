fe264860d95125319af93e7e6a031087219824eb
Gregory JSWhisperer <hello@jswhisperer.uk>
2026-04-01 07:09:32 +0100

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
