374c6c2faaa9242e02dac5b81df361372bed2369
imrofayel <adamrofayel@gmail.com>
2026-04-17 10:48:57 +0500

    feat: initialize Tauri application with Vue.js integration

---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite-plus/client" />
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
