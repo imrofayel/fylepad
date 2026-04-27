82272bbd6179d4fc2737c706989f066aa717312e
imrofayel <adamrofayel@gmail.com>
2026-04-27 15:09:15 +0500

    feat: update index.vue template to enhance layout and include favicon

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,1 +1,7 @@
-<template>Hello!</template>
+<template>
+  <div class="h-screen p-3">
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <img src="/favicon.svg" alt="fylepad logo" class="w-14 h-14 mx-auto mb-4" />
+    </div>
+  </div>
+</template>
