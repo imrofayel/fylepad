22178455c531c8c5863b848a44a19c19caa96f82
imrofayel <adamrofayel@gmail.com>
2026-04-28 13:48:47 +0500

    feat: add cursor pointer to UButton in index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="h-screen p-3">
     <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <img src="/favicon.svg" alt="fylepad logo" class="w-14 h-14 mx-auto mb-4" />
-      <UButton label="Open" to="/home" />
+      <UButton label="Open" to="/home" class="cursor-pointer" />
     </div>
   </div>
 </template>
