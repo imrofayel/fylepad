f825e71d99b963a272ba39f7601fd0d2e74bf503
imrofayel <adamrofayel@gmail.com>
2026-04-28 13:48:08 +0500

    feat: add navigation to home in UButton component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="h-screen p-3">
     <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <img src="/favicon.svg" alt="fylepad logo" class="w-14 h-14 mx-auto mb-4" />
-      <UButton label="Open" />
+      <UButton label="Open" to="/home" />
     </div>
   </div>
 </template>
