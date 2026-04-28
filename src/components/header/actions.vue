b87fcdd3659c43d6a22b61d661dfb4b184b172bc
imrofayel <adamrofayel@gmail.com>
2026-04-28 10:00:15 +0500

    feat: enhance header actions to conditionally render elements based on active tab

--- src/components/header/actions.vue
+++ src/components/header/actions.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import { useEditor } from "@/composables/useEditor";
 import { ICONS } from "@lib/constants/icons";
 import { useColorMode } from "@vueuse/core";
 
 const { value } = useColorMode();
+const { activeTabId } = useEditor();
 </script>
 <template>
-  <div class="flex items-center gap-2.5">
+  <div class="flex items-center gap-2.5" v-if="activeTabId">
     <EmojiPicker />
     <EditorCustomize />
     <ButtonWithTooltip text="Search (Ctrl + F)" :icon="ICONS.search" />
@@ -14,4 +16,8 @@ const { value } = useColorMode();
     </UTooltip>
     <HeaderMenu />
   </div>
+
+  <UTooltip :text="value === 'light' ? 'Dark mode' : 'Light mode'" arrow v-if="!activeTabId">
+    <UColorModeButton variant="link" color="neutral" />
+  </UTooltip>
 </template>
